b53b94b5b837bd83687a23ed3842cdbe8054e7e4
Cristiam Sosa <cristiam.sosa@innocv.com>
2019-01-08 12:29:20 +0100

    Initial commit

---
+++ src/test/java/com/innocv/multibucket/MultibucketApplicationTests.java
@@ -0,0 +1,17 @@
+package com.innocv.multibucket;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultibucketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
